11de10189f181d926f07b8e2d5433b8547af047f
Juan Barajas <juan.barajas4406@my.riohondo.edu>
2023-05-15 12:40:32 -0700

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+// 1st Test
+document.getElementById('main').remove()
+
+// 2nd Test
+const newHeader = document.createElement('h1');
+
+// 3rd Test
+newHeader.id = 'victory';
+
+// 4th Test
+newHeader.innerHTML = 'Juan Barajas is the champion'
